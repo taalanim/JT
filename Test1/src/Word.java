0976b1a985fb58ce63d0cd8b4fff051178efd14a
taalanim <jap13san@student.lu.se>
2017-09-01 23:51:31 +0200

    d

---
+++ Test1/src/Word.java
@@ -0,0 +1,17 @@
+
+public class Word {
+	private String jap;
+	private String eng;
+	public Word(String s){
+	// seperate the content	
+	}
+	public String GetJap(){
+		return "0";
+	}
+	
+	public String GetEng(){
+		
+		return "0";
+	}
+
+}
